5ebeb542ada5b0ad59a6cbb5a4a7504e622ee13b
ktnyt <kotone@sfc.keio.ac.jp>
2018-11-25 05:18:32 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ DESCRIPTION = 'POSIX fifo wrapper for Python'
 URL = 'https://github.com/ktnyt/pfifo'
 EMAIL = 'kotone@sfc.keio.ac.jp'
 AUTHOR = 'Kotone Itaya'
-VERSION = '1.0.4'
+VERSION = '1.0.5'
 
 SETUP_REQUIRED = ['pybind11>=2.2']
 
