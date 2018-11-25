e4fe5a045947431d66aa68ea5b206819fb557e70
ktnyt <kotone@sfc.keio.ac.jp>
2018-11-26 07:52:53 +0900

    Remove unnecessary includes

--- include/pfifo/reader.hpp
+++ include/pfifo/reader.hpp
@@ -11,8 +11,6 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#include <iostream>
-
 namespace pfifo {
 
 class reader {
--- include/pfifo/writer.hpp
+++ include/pfifo/writer.hpp
@@ -10,8 +10,6 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#include <iostream>
-
 namespace pfifo {
 
 class writer {
