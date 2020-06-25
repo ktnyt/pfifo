a02bbd6b61b79a2fc150a917f10ec11e8bcb02b5
kosuke miyoshi <miyoshi@narr.jp>
2020-06-25 13:30:52 +0900

    install the headers inside the package as well

--- pfifo/__init__.py
+++ pfifo/__init__.py
@@ -15,3 +15,14 @@ class Reader(_pfifo.Reader):
 class Writer(_pfifo.Writer):
     def __init__(self, name, mode=None):
         super(Writer, self).__init__(name, mode)
+
+
+def get_include():
+    import os
+    d = os.path.dirname(__file__)
+    if os.path.exists(os.path.join(d, "include")):
+        # Package is installed
+        return os.path.join(d, "include")
+    else:
+        # Package is from a source directory
+        return os.path.join(os.path.dirname(d), "include")
